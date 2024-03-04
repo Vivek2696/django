109aee0d3494636617d2ccc8f84d7b00de0eef05
Vivek Patel <patelvivek1126@gmail.com>
2024-03-03 23:36:46 -0600

    Adding more views

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Django Response')
+def january(request):
+    return HttpResponse('Eat no meat for the entire month! Better not to eat ever.')
+
+def february(request):
+    return HttpResponse('Walk for at least 20 minutes every day!')
