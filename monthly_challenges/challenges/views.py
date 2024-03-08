5991cbb602405a23b5d62ea3411139be97dc21f7
Vivek Patel <patelvivek1126@gmail.com>
2024-03-08 03:02:36 -0600

    Adding the path converter for month as integer and string

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+def monthly_challenges_by_number(request, month):
+    return HttpResponse(month)
+
 
 def monthly_challenge(request, month):
     challenge_text = None
